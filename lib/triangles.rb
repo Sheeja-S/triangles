7a03d38b397ed230bc65b9c2c7de246aee7f567e
Sheeja Sankar <sheeja.s18@gmail.com>
2017-03-27 12:01:51 +0800

    added not triangle method to check if side measurements are of a triangle

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -19,4 +19,10 @@ class Triangle
   define_method(:scalene?) do
     @side1.!=(@side2) && @side2.!=(@side3)
   end
+
+  define_method(:not_triangle?) do
+   (@side1.+(@side2)) <= @side3 ||    # if A + B < C or
+   (@side2.+(@side3)) <= @side1 ||    # if B + C < A or
+   (@side3.+(@side1)) <= @side2       # if C + A < B
+ end
 end
--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -36,5 +36,13 @@ describe('Triangle') do
       test_triangle = Triangle.new(6,7,9)
       expect(test_triangle.scalene?()).to(eq(true))
     end
+
+  describe('#not_triangle') do
+    it ("returns true when the sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side") do
+      test_triangle = Triangle.new(7, 5, 12)
+      expect(test_triangle.not_triangle?()).to(eq(true))
+    end
+  end
+
   end
 end
